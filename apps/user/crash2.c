aec72a19bccb2278ab9244ab4619a264360fe6b3
Nebiyu Hiskeal <nhiskeal@gmail.com>
2023-03-28 19:38:11 -0400

    completed

--- apps/user/crash2.c
+++ apps/user/crash2.c
@@ -13,7 +13,8 @@
 #include <string.h>
 
 int main() {
-    memset((void*)FRAME_CACHE_START, 0, FRAME_CACHE_END - FRAME_CACHE_START);
+    // memset((void*)FRAME_CACHE_START, 0, FRAME_CACHE_END - FRAME_CACHE_START);
+    memset((void*)0x08000000, 0, 0x27FF);
     /* If the OS protects memory correctly,
      * this memset should trigger an exception, killing this application;
      * Otherwise, the following message will be printed
